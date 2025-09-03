946f9fb27c2a1b19fe8cc72c012967e721904137
AmirMosafery <amirm4a112@gmail.com>
2025-09-04 01:09:19 +0330

    Create models

--- CoffeeMenu/models.py
+++ CoffeeMenu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(max_length=150)
+    price = models.IntegerField(default=0)
+    offer_price = models.FloatField(default=0)
+    offer_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
